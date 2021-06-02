b09eecbcb1011986b11f0fbf5e9edff1222712b1
Victor Mendonca <victor.men.dev@gmail.com>
2021-06-02 13:43:30 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -33,3 +33,23 @@ function trocaCorBlue(aleatory){
     selectBlue.classList.add('selected');
     console.log(aleatory.target);
 }
+
+let pixelSelect = document.querySelectorAll('.pixel');
+
+for(let index = 0; index < pixelSelect.length; index += 1){
+
+    pixelSelect[index].addEventListener('click', selecionaPixel);
+
+    function selecionaPixel (aleatory){
+        console.log(aleatory.target);
+        if(document.querySelector('.selected').id === 'red'){
+            pixelSelect[index].style.backgroundColor = 'red';
+        } else if(document.querySelector('.selected').id === 'black'){
+            pixelSelect[index].style.backgroundColor = 'black';
+        } else if(document.querySelector('.selected').id === 'purple'){
+            pixelSelect[index].style.backgroundColor = 'purple';
+        } else if(document.querySelector('.selected').id === 'blue'){
+            pixelSelect[index].style.backgroundColor = 'blue';
+        }
+    }
+}
